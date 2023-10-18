77d60c88918b46db3d5ce736481e30e68c506833
Vipul Prasad <vipulprasadm@gmail.com>
2023-10-18 15:18:49 +0530

    new codes to read data

---
+++ read_abacus_cosmos.py
@@ -0,0 +1,97 @@
+"""
+Here halo mass is provided as number of particles in a halo,
+position is given as -550 to +550 Mpc
+
+ ParticleMassMsun =     5.411408436271152e+10
+ ParticleMassHMsun =     3.731168281372334e+10
+ 
+ BoxSizeMpc = 1595.358030249148442
+ BoxSizeHMpc = 1100.000000000000000"""
+
+import numpy as np
+import random
+
+#halo mass is provided as number of particles
+particle_mass_real = 5.776643562630781e+10 #Msun
+particle_mass =     3.731168281372334e+10 #hMsun
+boxsize = 1100.000000000000000 #hMPc
+boxsize_real = 1635.444543562294939 #MPc
+
+
+#struct form of the stored data
+halo_dt_FoF = lambda: np.dtype([("id",np.int64),("subsamp_start",np.uint64),("subsamp_len",np.uint32),
+               ("N",np.uint32),("subhalo_N",np.uint32,4),("pos",np.float32,3),("vel",np.float32,3),("sigma_v",np.float32,3),
+               ("r25",np.float32),("r50",np.float32),("r75",np.float32),("r90",np.float32),
+               ("vcirc_max",np.float32),("rvcirc_max",np.float32),
+               ("subhalo_pos",np.float32,3),("subhalo_vel",np.float32,3),("subhalo_sigma_v",np.float32,3),
+               ("subhalo_r25",np.float32),("subhalo_r50",np.float32),("subhalo_r75",np.float32),("subhalo_r90",np.float32),
+               ("subhalo_vcirc_max",np.float32),("subhalo_rvcirc_max",np.float32)], align=True)
+
+def format_e(n):
+    a = '%.2E' % n
+    return a.split('E')[0].rstrip('0').rstrip('.') + 'E' + a.split('E')[1]
+
+
+def read_file(filename, rand = 1):
+    # rand should be a value between 0 and 1, fraction for random sampling
+	with open(filename,"rb") as fp:
+		
+		#reading to structs
+		num_groups = np.fromfile(fp,dtype=np.uint64,count=1)
+		n_largest_subhalos = np.fromfile(fp,dtype=np.uint64,count=1)
+		assert (n_largest_subhalos,) == halo_dt_FoF()['subhalo_N'].shape, n_largest_subhalos
+		data = np.fromfile(fp, dtype=halo_dt_FoF())
+		assert len(data) == num_groups, (len(data), num_groups, halo_fn)
+		
+		#taking number of halo particles and position
+		data = data[["N","pos"]]#N is the number of particles in a halos 
+		
+		if rand != 1:#ramodom sampling
+		    random_data = []
+		    N = len(data)
+		    index = random.sample(range(N), int(rand*N)) # 0.1 gives 10% data
+		    for i in index:
+			    random_data.append(data[i])
+		    data = random_data
+            del random_data#just to clear the variable save ram
+
+		mass = []
+		pos = []
+		
+		for i in range(len(data)):
+			mass.append(data[i][0]*particle_mass)
+			pos.append(data[i][1]+550) # +550 is to make the box coordinates between 0 and 1100Mpc
+		
+		del data
+		return np.column_stack((mass, pos))	
+
+def create_mass_slice(data, ml_cut, mu_cut):
+
+        outfile_file = open(massbin_filenames_file, 'a')
+
+        if rand_sample != 0:
+                rand_index = np.random.randint(0, len(data), rand_sample)
+                data = data[rand_index]
+                out_fname = "halos_mcut_{}_{}_{}_sample.txt".format(format_e(ml_cut), format_e(mu_cut), format_e(rand_sample))
+                outfile_file.write(out_fname+'\n')
+                print(out_fname)
+        else:
+                out_fname = "halos_mcut_{}_{}_.txt".format(format_e(ml_cut), format_e(mu_cut))
+                outfile_file.write(out_fname+'\n')
+                print(out_fname)
+
+        outfile_file.close()
+
+        data = data[(data['N'] > ml_cut) & (data['N'] <= mu_cut)]
+
+        print(len(data))
+
+        data.to_csv(output_path+out_fname, sep = ',', mode = 'a', index = False, header = False)
+        return len(data)
+
+halo_cat = [read_file('halos_{}'.format(i)) for i in range(4)]	
+		
+np.savetxt("halos.txt", np.concatenate((halo_cat[0], halo_cat[1], halo_cat[2], halo_cat[3])))
+
+
+
---
+++ read_abacus_summit.py
@@ -0,0 +1,101 @@
+import numpy as np
+from abacusnbody.data.compaso_halo_catalog import CompaSOHaloCatalog
+import matplotlib.pyplot as plt
+import scienceplots
+plt.style.use('science')
+import os
+
+def format_e(n):
+    a = '%.2E' % n
+    return a.split('E')[0].rstrip('0').rstrip('.') + 'E' + a.split('E')[1]
+
+def readdata(file_name, clms):
+
+        print("Reading the data")
+
+        cat = CompaSOHaloCatalog(file_name,cleaned=False)
+        data = cat.halos[clms]
+        data['N'] = data['N'].astype('float')*part_mass
+        data['SO_central_particle'] += (box_size/2) # correcting the box coordinates origin
+        data['X'] =data['SO_central_particle'][:,0]
+        data['Y'] =data['SO_central_particle'][:,1]
+        data['Z'] =data['SO_central_particle'][:,2]
+        del cat
+        return data[['N', 'X', 'Y', 'Z']].to_pandas()
+
+def data_summary(data):
+
+        bins = np.logspace(np.log10(min(data['N'])), np.log10(max(data['N'])), 100)
+        bin_mid = [(bins[i]+bins[i+1])/2 for i in range(len(bins)-1)]
+        plt.figure(figsize = (8,8))
+        num, _ = np.histogram(data['N'], bins = bins)
+        plt.scatter(bin_mid, 34*num)
+        plt.xscale("log")
+        plt.yscale("log")
+        plt.xlabel("M")
+        plt.ylabel("Number of halos")
+        plt.show()
+
+        print('tot particles = ' , len(data))
+        print('min mass =', format_e(min(data['N'])))
+        print('max mass =', format_e(max(data['N'])))
+
+def create_mass_slice(data, ml_cut, mu_cut):
+
+        outfile_file = open(massbin_filenames_file, 'a')
+
+        if rand_sample != 0:
+                rand_index = np.random.randint(0, len(data), rand_sample)
+                data = data[rand_index]
+                out_fname = "halos_mcut_{}_{}_{}_sample.txt".format(format_e(ml_cut), format_e(mu_cut), format_e(rand_sample))
+                outfile_file.write(out_fname+'\n')
+                print(out_fname)
+        else:
+                out_fname = "halos_mcut_{}_{}_.txt".format(format_e(ml_cut), format_e(mu_cut))
+                outfile_file.write(out_fname+'\n')
+                print(out_fname)
+
+        outfile_file.close()
+
+        data = data[(data['N'] > ml_cut) & (data['N'] <= mu_cut)]
+
+        print(len(data))
+
+        data.to_csv(output_path+out_fname, sep = ',', mode = 'a', index = False, header = False)
+        return len(data)
+
+def load_files(mass_list):
+        # To do -> Create seperate mass list
+        halo_files = os.listdir(input_path)
+        
+        for file in halo_files[0:2]:
+                if file[0:4] == 'halo':
+                        open(massbin_filenames_file, 'w').close()
+                        print(file)
+                        halo_cat = readdata(input_path+file, ['N', 'SO_central_particle'])
+                        
+                        for i in range(len(mass_list)-1):
+                                print(create_mass_slice(halo_cat, mass_list[i], mass_list[i+1]))
+                        del halo_cat
+
+#---------------------------------------------------------------------------#
+
+massbin_filenames_file = 'file_names_7500.txt'
+max_halo_mass = 147886687132648.38
+min_halo_mass = 1993082036828.1453
+
+h = 1#0.674
+
+#part_mass = 3.131059264330557e+09*h
+#part_mass = 2.109081520453063e+09 #hmsun 500
+part_mass = 5.694520105223272e+10 #hmsun 7500
+box_size = 7500 # hmpc
+rand_sample = 0 #int(1e5)
+output_path = "/home/vipul/vipul/halo_clutering/bias_calc/box7500/z3.0/halo_cat/" # path to mass binned halo catalogs
+input_path = "/home/vipul/vipul/halo_clutering/AbacusSummit_huge_c000_ph201/halos/z3.000/halo_info/" # path to halo catalog file directory
+
+mass_list = [2e12, 2.2e12, 2.4e12, 2.6e12, 2.8e12, 3.2e12, 4e12, 6e12, 10e12, max_halo_mass]
+#mass_list = [(3e12)*(5**i) for i in range(3)]+[max_halo_mass]
+load_files(mass_list)
+
+
