43b4ce40619dccf208fa406e13f613b01261d203
Vipul <vipulprasadm@gmail.com>
2023-10-22 20:08:35 +0530

    halo exclusion scale

--- halo_exclusion.py
+++ halo_exclusion.py
@@ -7,13 +7,13 @@ halo_mass = 147886687132648.38 # mass of smallest halo solar mass
 
 omega_m = 0.301685                  # Matter(baryons+dark matter) density parameter.
 Mpc = 3.0857e22                     # 1 Mpc in metres.
-sm = 2e30                           # Solar mass in Kg.
-G = 6.67408e-11*sm/(Mpc**3)         # Gravitational constant.
-h = 0.674                   # Hubble constant in units of 100 Km/s/Mpc.
+sm = 1.98e30                           # Solar mass in Kg.
+G = 6.67408e-11#*sm/(Mpc**3)         # Gravitational constant.
+h = 0.678                   # Hubble constant in units of 100 Km/s/Mpc.
 H_o = (100*h)/(Mpc*(10**(-3)))      # Hubble constant.
-rho_crit = (3*(H_o)**2)/(8*pi*G) # critical density of the universe.
+rho_crit = (3*(H_o)**2)/(8*pi*G)*(Mpc**3/sm) # critical density of the universe.
 rho_bar = omega_m*rho_crit          # comoving background density in kg/m^3.
 
 
-scale = pow((3*halo_mass*sm)/(4*pi*delta*rho_crit), 1/3)
-print(scale)
+scale = pow((3*halo_mass)/(4*pi*delta*rho_crit), 1/3)
+print("Halo exclusion scale = {} MPc".format(scale))
